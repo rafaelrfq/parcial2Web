785db05e18d7e88c947d7d93b7ccc1fed1c49d8b
Rafael <rafaelrfq@gmail.com>
2020-07-21 23:28:15 -0400

    Conexion para test

--- src/main/java/edu/pucmm/eict/Main.java
+++ src/main/java/edu/pucmm/eict/Main.java
@@ -1,7 +1,24 @@
 package edu.pucmm.eict;
 
-public class Main {
-    public static void main(String[] args) {
+import edu.pucmm.eict.controladora.DataBaseServices;
+import io.javalin.Javalin;
+import io.javalin.core.util.RouteOverviewPlugin;
 
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        // Se inicia la base de datos
+        DataBaseServices.getInstancia().startDB();
+
+        // Se prueba la conexion con la DB
+        DataBaseServices.getInstancia().testConn();
+
+        Javalin app = Javalin.create(javalinConfig -> {
+            javalinConfig.addStaticFiles("/public"); //Agregamos carpeta public como source de archivos estaticos
+            javalinConfig.registerPlugin(new RouteOverviewPlugin("rutas")); //Aplicamos el plugin de rutas
+        }).start(7000);
+
+        DataBaseServices.getInstancia().stopDB();
     }
 }
