d6c0e3569619f8ba77c7690d36594d744ce7b812
John Lopez <johncarlos1943@gmail.com>
2020-07-21 22:53:44 -0400

    ok 2

--- src/main/java/edu/pucmm/eict/logico/Usuario.java
+++ src/main/java/edu/pucmm/eict/logico/Usuario.java
@@ -1,4 +1,48 @@
 package edu.pucmm.eict.logico;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
+    @Id
+    private String usuario;
+    @Column(name = "Nombre")
+    private String nombre;
+    @Column(name = "Password")
+    private String password;
+
+    public Usuario(){
+
+    }
+    public Usuario(String usuario, String nombre, String password) {
+        this.usuario = usuario;
+        this.nombre = nombre;
+        this.password = password;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
