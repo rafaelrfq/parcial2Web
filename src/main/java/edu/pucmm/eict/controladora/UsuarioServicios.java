e279c8e8bcc3492e50bb21b49bcb08fac875963f
John Lopez <johncarlos1943@gmail.com>
2020-07-21 23:48:22 -0400

    usuario

---
+++ src/main/java/edu/pucmm/eict/controladora/UsuarioServicios.java
@@ -0,0 +1,10 @@
+package edu.pucmm.eict.controladora;
+
+import edu.pucmm.eict.logico.Usuario;
+
+public class UsuarioServicios extends GestionadDB<Usuario>  {
+
+    public UsuarioServicios() {
+        super(Usuario.class);
+    }
+}
