58600b877d6101467cfe06245b33e0fe2c0ed971
Rafael <rafaelrfq@gmail.com>
2020-07-21 23:55:49 -0400

    test

--- src/main/java/edu/pucmm/eict/Main.java
+++ src/main/java/edu/pucmm/eict/Main.java
@@ -1,6 +1,8 @@
 package edu.pucmm.eict;
 
 import edu.pucmm.eict.controladora.DataBaseServices;
+import edu.pucmm.eict.controladora.UsuarioServicios;
+import edu.pucmm.eict.logico.Usuario;
 import io.javalin.Javalin;
 import io.javalin.core.util.RouteOverviewPlugin;
 
@@ -13,6 +15,10 @@ public class Main {
 
         // Se prueba la conexion con la DB
         DataBaseServices.getInstancia().testConn();
+
+        // Se agregan usuarios de prueba
+        Usuario tmp = new Usuario("admin", "Administradora", "admin");
+        UsuarioServicios.getInstance().crear(tmp);
 
         Javalin app = Javalin.create(javalinConfig -> {
             javalinConfig.addStaticFiles("/public"); //Agregamos carpeta public como source de archivos estaticos
